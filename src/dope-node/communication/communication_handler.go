f1540ba9cb1b5d8b7e19912bfeff4230f14ed808
Lukasz Nowakowski <lukrzak@wp.pl>
2024-10-22 16:25:46 +0200

    Added node discovery mechanism

--- src/dope-node/communication/communication_handler.go
+++ src/dope-node/communication/communication_handler.go
@@ -6,20 +6,37 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"strconv"
 
 	"github.com/gorilla/websocket"
 )
 
-func RegisterEndpoints(boostrapServerAddress string, isBoostrapServer bool) {
+var knownAddresses = []string{}
+
+func RegisterEndpoints(boostrapServerAddress string, isBoostrapServer bool, port int) {
 	http.HandleFunc("/ws", wsHandler)
+	log.Println("Running server")
 	if isBoostrapServer {
 		http.HandleFunc("/addresses", httpHandler)
 	} else {
-		nodeAddresses := fetchNodeAddresses
-		log.Println(nodeAddresses)
+		// This sends 'hello' message to all known nodes
+		addresses := fetchNodeAddresses(boostrapServerAddress, "127.0.0.1:"+strconv.Itoa(port))
+		for _, addr := range addresses {
+			u := url.URL{Scheme: "ws", Host: addr, Path: "/ws"}
+			conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+			if err != nil {
+				log.Printf("Failed to connect to %s: %v", addr, err)
+				continue
+			}
+			defer conn.Close()
+
+			messageToSend, _ := json.Marshal(Message{Content: "hello"})
+			sendMessage(*conn, messageToSend)
+		}
 	}
 
-	http.ListenAndServe(":7312", nil)
+	http.ListenAndServe(":"+strconv.Itoa(port), nil)
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,19 +45,36 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	for {
 		_, msg, err := connection.ReadMessage()
 		if err != nil {
-			log.Fatal("Error reading message:", err)
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Println("WebSocket closed unexpectedly:", err)
+			} else {
+				log.Println("Error reading message:", err)
+			}
 			break
 		}
 
-		log.Println("Handling websocket message from: " + r.Host)
+		log.Println("Handling WebSocket message from:", r.Host)
 		var receivedMessage Message
-		json.Unmarshal(msg, &receivedMessage)
-		fmt.Println(receivedMessage.Content)
+		err = json.Unmarshal(msg, &receivedMessage)
+		if err != nil {
+			log.Println("Error unmarshalling message:", err)
+			continue
+		}
+		fmt.Println("Received message content:", receivedMessage.Content)
 	}
 }
 
 func httpHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Handling http request from: " + r.Host)
+	log.Println("Handling http request")
+	addressesResponse := FetchBootstrapAddressesResponse{
+		Addresses: knownAddresses,
+	}
+
+	ip := r.URL.Query().Get("sender")
+	log.Println("Added address: " + ip)
+	knownAddresses = append(knownAddresses, ip)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(addressesResponse)
 }
 
 func getWebsocketConnection(w http.ResponseWriter, r *http.Request) *websocket.Conn {
@@ -50,24 +84,23 @@ func getWebsocketConnection(w http.ResponseWriter, r *http.Request) *websocket.C
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal("Error upgrading to websocket:", err)
-		conn.Close()
-	} else {
-		log.Println("Connection handled successfully")
+		log.Fatal("Error upgrading to WebSocket:", err)
+		return nil
 	}
 
 	return conn
 }
 
-func sendMessage(connection websocket.Conn, response []byte) {
-	status := connection.WriteMessage(websocket.TextMessage, response)
-	if status != nil {
-		log.Println("Error writing message:", status)
+func sendMessage(connection websocket.Conn, mess []byte) {
+	err := connection.WriteMessage(websocket.TextMessage, mess)
+	if err != nil {
+		log.Println("Error writing message:", err)
+		return
 	}
 }
 
-func fetchNodeAddresses(bootstrapSeverAddress string) []string {
-	resp, err := http.Get("http://" + bootstrapSeverAddress + "/addresses")
+func fetchNodeAddresses(bootstrapSeverAddress string, addressToRegister string) []string {
+	resp, err := http.Get("http://" + bootstrapSeverAddress + "/addresses?sender=" + addressToRegister)
 	if err != nil {
 		log.Fatal("Error making the GET request:", err)
 	}
