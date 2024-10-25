e7eb17ef4cb594558c08d036ddc065ba40f66e61
tpalinski <tpalinski@icloud.com>
2024-10-25 10:47:00 +0200

    [chore] Connect storage lib

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"dope-node/config"
 
 	log "github.com/sirupsen/logrus"
+	db "github.com/DopamineInjector/go-dope-db"
 )
 
 func main() {
@@ -13,4 +14,11 @@ func main() {
 		log.Warn("Could not find read config.toml, resolving to default config values");
 	}
 	log.Info("Parsed node configuration")
+	log.Info("Connecting to node storage")
+	dbUrl := config.GetString(config.DbUrlKey);
+	checksum, err := db.GetChecksum(dbUrl);
+	if err != nil {
+		log.Fatalf("Could not connect to db instance, exiting\n%s", err.Error());
+	}
+	log.Infof("Connected to storage, current state checksum: %s", checksum.Checksum);
 }
