f49e189e026f6c8e29eece90131bd469e3505eea
Lukasz Nowakowski <lukrzak@wp.pl>
2024-12-02 20:56:49 +0100

    Changed signature implementation

--- communication/smartContractHandler.go
+++ communication/smartContractHandler.go
@@ -15,12 +15,12 @@ func handleSmartContract(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		var input struct {
 			Payload struct {
-				Sender     string `json:"sender"`
+				Sender     []byte `json:"sender"`
 				Contract   string `json:"contract"`
 				Entrypoint string `json:"entrypoint"`
 				Args       string `json:"args"`
 			} `json:"payload"`
-			Signature string `json:"signature"`
+			Signature []byte `json:"signature"`
 			View      bool   `json:"view"`
 		}
 		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
@@ -31,7 +31,11 @@ func handleSmartContract(w http.ResponseWriter, r *http.Request) {
 			Output string `json:"output"`
 		}
 
-		utils.VerifySignature(input.Signature, fmt.Sprintf("%v", input.Payload), input.Signature)
+		marshalledPayload, err := json.Marshal(input.Payload)
+		if err != nil {
+			log.Warnf("error marshalling payload")
+		}
+		utils.VerifySignature([]byte(input.Payload.Sender), string(marshalledPayload), input.Signature)
 
 		if input.View {
 			cmd := exec.Command("/bin/dopechain-vm", "-e", input.Payload.Entrypoint, "-a", input.Payload.Args, "-s", input.Payload.Sender, "--block-number", fmt.Sprintf("%d", len(blockchain.DopeChain)-1))
