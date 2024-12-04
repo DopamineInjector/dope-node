8dcff5d523b60e7a4caa77f34b203bdeb3c050cd
tpalinski <tpalinski@icloud.com>
2024-12-04 10:21:41 +0100

    add prayer for engineers

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
+// Almighty Father, You are the source of all wisdom, talents, and skills.
+// Thank You for these beautiful gifts, opportunities to serve you, and the work that You have entrusted to me.
+// Please open my heart and enlighten my mind, so that I may be fully in tune with Your divine purpose in calling me to the engineering profession.
+// Lord God, You are the greatest engineer. Please infuse me even with just the tiniest spark of Your Divine Wisdom so that as I  do my work, it is really your work that is done.
+// Loving God, make my heart Your Heart, make my mind Your Mind, and make my hands Your Hands.
+// Make me your instrument, so that I may be always conscious and mindful of the fact that on my work depend the lives and properties of my fellow human. Bless me also with the gift of love and sensitivity to respect the people who build and use the products of my work.
+// This I ask in Jesus’ name.
+// AMEN.
 package main
 
 import (
